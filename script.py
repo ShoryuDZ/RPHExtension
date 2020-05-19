20603e9c9ee52b771c21cd1e6db04bfaa688eedf
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-05-19 21:14:40 +1000

    Parsing URLs
    Allows all videos from playlist to have URLs appended into sites.json

---
+++ script.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import json
+
+url = 'https://www.pornhub.com/playlist/154702881'
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+
+videosTiles = soup.find_all("a", class_="fade")
+
+with open('sites.json') as json_file:
+    sites = json.load(json_file)
+
+for video in videosTiles:
+    sites["siteList"].append(video.get("href"))
+
+with open('sites.json', 'w') as json_file:
+    json.dump(sites, json_file)
