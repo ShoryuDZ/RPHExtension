e40feb0fc08bd87bd705ef660c52c230ae22fefc
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-06-25 23:35:32 +1000

    Running & Opening at Random
    A random website will open once every 6 hours...

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
-function randomButton(site) {
-    chrome.windows.create({"url": site, "incognito": true});
+var cycleTime = 12 * 60 * 1000;
+setInterval(openAtRandom, cycleTime);
+
+function openAtRandom() {
+    var randomTime = Math.random() * cycleTime;
+    setTimeout(openRandomSite, randomTime);
 }
+
+function openRandomSite() {
+    var sites = [];
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        site = JSON.parse(this.responseText);
+        chrome.windows.create({"url": site, "incognito": true});
+    }
+    };
+    xmlhttp.open("GET", "http://vd20.pythonanywhere.com/randomsite", true);
+    xmlhttp.send();
+}
+
--- popup.js
+++ popup.js
@@ -7,15 +7,5 @@ function clickHandler(e) {
 }
 
 function randomButton() {
-    var sites = [];
-
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        site = JSON.parse(this.responseText);
-        chrome.extension.getBackgroundPage().randomButton(site);
-    }
-    };
-    xmlhttp.open("GET", "http://vd20.pythonanywhere.com/randomsite", true);
-    xmlhttp.send();
+    chrome.extension.getBackgroundPage().openRandomSite();
 }
